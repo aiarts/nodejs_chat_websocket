b5463a0db5ab438fc47ea61ebd0abe34f414c200
lkk <wumiller88@gmail.com>
2023-12-31 00:54:09 +0800

    hhtps test ok

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ io.on('connection', (socket) => {
 // 監聽 port
 var port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log("Server Started. http://localhost:3000");
+    console.log("Server Started. https://localhost");
 }); 
 
